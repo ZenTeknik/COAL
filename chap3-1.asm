f714c58be504d190d2ed0178e23ce4ee055801c6
Haris <144903716+haris78866@users.noreply.github.com>
2024-10-05 13:21:37 +0500

    Add files via upload

---
+++ chap3-1.asm
@@ -0,0 +1,23 @@
+[org 0x100]
+
+xor ax,ax
+xor bx,bx
+
+l1 :   mov ax,[num1+bx]       ; base + offset address 
+        add bx,2
+        cmp bx,20;
+        jne l1
+        
+      mov [total],ax
+
+
+xor ax,ax
+xor bx,bx
+
+      mov ax,0x4c00
+      int 0x21
+  
+  
+  num1 : dw     1, 2, 3, 4, 5, 6, 7, 8, 9
+  total : dw    0
+
